9533a85cf0f91a6fb900bc09ea1c132022728877
naeemi7 <abdulwasenaeemi7@gmail.com>
2023-10-17 12:45:11 +0200

    bug fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,21 @@
-import { StatusCodes } from "http-status-codes"; // Import StatusCodes directly
+import { StatusCodes } from "http-status-codes";
 import User from "../models/User.js";
-import bcrypt from "bcrypt"; // Import bcrypt properly
-import { hashSync } from "bcrypt"; // Import the hashSync function if needed
+import bcrypt from "bcrypt";
 
 /**
- * Function to create User
- * @param {*} req
- * @param {*} res
- * @returns
+ * Create User: Handles user registration
+ * @param {*} req - Express request object
+ * @param {*} res - Express response object
+ * @returns - JSON response
  */
 export const createUser = async (req, res) => {
-  // Hash the password
+  // Hash the user's password
   const hashedPassword = await bcrypt.hash(req.body.password, 12);
 
   try {
     const { firstname, lastname, email } = req.body;
 
+    // Create a new user in the database
     const newUser = await User.create({
       firstname,
       lastname,
@@ -24,17 +24,20 @@ export const createUser = async (req, res) => {
     });
 
     if (!newUser) {
+      // Return a bad request response if user creation fails
       return res.status(StatusCodes.BAD_REQUEST).json({
         error: "You need to provide the required information to sign up!",
       });
     }
 
+    // Return a success response with the newly created user
     return res.status(StatusCodes.CREATED).json({
       message: "User created successfully!",
       newUser,
     });
   } catch (error) {
     console.error(error); // Log the error for debugging
+    // Return an internal server error response if an error occurs
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: "Something went wrong!",
     });

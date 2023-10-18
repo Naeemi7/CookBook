d32b0278fad47b23ad91136797894b65dd189f3b
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-18 06:14:43 +0200

    bug fixed

--- backend/middleware/authorization.js
+++ backend/middleware/authorization.js
@@ -4,22 +4,21 @@ import { verifyJwt } from "../helper/jwt.js";
 export const authorizeUser = (req, res, next) => {
   // Handle if the token isn't there
   if (!req.cookies.jwt) {
+    //does the jwt token cookie exist?
     return res
       .status(StatusCodes.UNAUTHORIZED)
-      .json({ error: "UNAUTHORIZED!" });
+      .json({ message: "Unauthorized" }); //it doesn't exist
   }
 
+  //verify if token is valid
   try {
-    const isValid = verifyJwt(req.cookies.jwt); // Correct the typo "req.cookes" to "req.cookies"
-    if (!isValid) {
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({ error: "UNAUTHORIZED!" });
-    }
+    const isValid = verifyJwt(req.cookies.jwt);
+    console.log("isValid token", isValid);
   } catch (error) {
     return res
       .status(StatusCodes.UNAUTHORIZED)
-      .json({ error: "UNAUTHORIZED!" });
+      .json({ errorCode: 401, message: "Unauthorized" });
   }
+
   next();
 };

97186357e4e4d6ebd1e0179d26cc21475b9827b1
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-19 12:20:59 +0200

    bug fixed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -19,7 +19,6 @@ const Register = () => {
                 name="fullname"
                 id="fullname"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-cards dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@company.com"
                 required
               />
             </div>
@@ -35,7 +34,6 @@ const Register = () => {
                 name="email"
                 id="email"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-cards dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@company.com"
                 required
               />
             </div>
@@ -51,7 +49,6 @@ const Register = () => {
                 type="password"
                 name="password"
                 id="password"
-                placeholder="••••••••"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-cards dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required
               />
@@ -67,14 +64,13 @@ const Register = () => {
                 type="password"
                 name="confirm-password"
                 id="confirm-password"
-                placeholder="••••••••"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-cards dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required
               />
             </div>
             <button
               type="submit"
-              className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover-bg-primary-700 dark:focus:ring-primary-800"
+              className="w-full text-button bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover-bg-primary-700 dark:focus:ring-primary-800"
             >
               Create an account
             </button>
@@ -82,7 +78,7 @@ const Register = () => {
               Already have an account?{" "}
               <a
                 href="#"
-                className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                className="font-medium text-button hover:underline dark:text-primary-500"
               >
                 Login here
               </a>

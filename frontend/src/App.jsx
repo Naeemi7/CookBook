2494feccfb9b9ae8a3326e2b38f1d28cdab1072d
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-19 12:53:24 +0200

    login button has been fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <Navbar />
-      <Register />
-      {/*    <Login /> */}
+      {/*  <Register /> */}
+      <Login />
       <Footer />
     </>
   );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
 
             <button
               type="submit"
-              className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover-bg-primary-700 dark:focus:ring-primary-800"
+              className="w-full text-cards bg-button hover:text-button hover:bg-footer focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover-bg-primary-700 dark:focus:ring-primary-800"
             >
               Sign in
             </button>

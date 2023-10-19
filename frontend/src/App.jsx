c941f32d2b6fe93801eec9f50ccc82cc2a569d8c
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-19 12:52:32 +0200

    registeration page has been created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Navbar />
       <Register />
+      {/*    <Login /> */}
       <Footer />
     </>
   );
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -19,6 +19,7 @@ const Register = () => {
                 name="fullname"
                 id="fullname"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-cards dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="John Doe"
                 required
               />
             </div>
@@ -34,6 +35,7 @@ const Register = () => {
                 name="email"
                 id="email"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-cards dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name@company.com"
                 required
               />
             </div>
@@ -50,6 +52,7 @@ const Register = () => {
                 name="password"
                 id="password"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-cards dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="••••••••"
                 required
               />
             </div>
@@ -65,12 +68,13 @@ const Register = () => {
                 name="confirm-password"
                 id="confirm-password"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-cards dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="••••••••"
                 required
               />
             </div>
             <button
               type="submit"
-              className="w-full text-button bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover-bg-primary-700 dark:focus:ring-primary-800"
+              className="w-full text-cards bg-button hover:text-button hover:bg-footer focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover-bg-primary-700 dark:focus:ring-primary-800"
             >
               Create an account
             </button>

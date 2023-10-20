ea7f8a6af0e84ea616906692061e7d3eff39f260
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-20 06:32:28 +0200

    bug fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -250,7 +250,7 @@ const Navbar = () => {
         </div>
 
         {/* Burger Menu Button */}
-        <button
+        {/* <button
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-00 rounded-lg md:hidden hover-bg-gray-100 focus-outline-none focus-ring-2 focus-ring-gray-200 dark-text-gray-400 dark-hover-bg-gray-700 dark-focus-ring-gray-600"
           onClick={() => setMenuOpen(!isMenuOpen)}
@@ -269,7 +269,7 @@ const Navbar = () => {
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
-        </button>
+        </button> */}
       </div>
     </nav>
   );

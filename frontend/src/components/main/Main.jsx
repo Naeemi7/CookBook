78f7a24193fc6e820d73349aa4c167006a1e79a9
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-22 16:49:39 +0200

    bug fixed

---
+++ frontend/src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+/* import Navbar from "../Navbar/Navbar";
+import Recipe from "../Recipe";
+import "../../styles/main.scss";
+const Main = () => {
+  return (
+    <main className="main-section">
+      <Navbar />
+      <Recipe />
+    </main>
+  );
+};
+
+export default Main;
+ */

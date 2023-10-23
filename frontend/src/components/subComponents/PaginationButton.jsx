5cc6cc20bdb2f6cbb35e8408fbd8d102046be604
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-23 15:25:06 +0200

    bug fixed

--- frontend/src/components/subComponents/PaginationButton.jsx
+++ frontend/src/components/subComponents/PaginationButton.jsx
@@ -13,73 +13,81 @@ const PaginationButton = () => {
   const isFirstPage = skip === 0;
   const isLastPage = skip + limit >= recipe.length;
 
-  return recipe ? (
-    <ul className="flex items-center -space-x-px mt-12 h-8 text-sm">
-      <li>
-        <a
-          href="#"
-          className={`flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-white ${
-            isFirstPage ? "inactive-prev" : "active-prev"
-          }`}
-          onClick={() => handlePageChange(skip - limit)}
-        >
-          <span className="sr-only">Previous</span>
-          <svg
-            className="w-2.5 h-2.5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-        </a>
-      </li>
-      {Array.from({ length: totalPages }).map((_, index) => (
-        <li key={index}>
-          <a
-            href="#"
-            onClick={() => handlePageChange(index * limit)}
-            className={"bg-btton"}
-          >
-            {currentPage}
-          </a>
-        </li>
-      ))}
-      <li>
-        <a
-          href="#"
-          className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-button border border-gray-300 rounded-r-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-white ${
-            isLastPage ? "inactive-next" : "active-next"
-          }`}
-          onClick={() => handlePageChange(skip + limit)}
-        >
-          <span className="sr-only">Next</span>
-          <svg
-            className="w-2.5 h-2.5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-        </a>
-      </li>
-    </ul>
-  ) : null;
+  return (
+    <>
+      {recipe.length > 0 ? (
+        <ul className="flex items-center -space-x-px mt-12 h-8 text-sm">
+          <li>
+            <a
+              href="#"
+              className={`flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-gray-700 ${
+                isFirstPage ? "inactive-prev" : "active-prev"
+              }`}
+              onClick={() => handlePageChange(skip - limit)}
+            >
+              <span className="sr-only">Previous</span>
+              <svg
+                className="w-2.5 h-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+            </a>
+          </li>
+          {Array.from({ length: totalPages }).map((_, index) => (
+            <li key={index}>
+              {currentPage !== index + 1 ? ( // Changed the condition to compare with the current page
+                <a
+                  href="#"
+                  onClick={() => handlePageChange(index * limit)}
+                  className={
+                    "flex items-center justify-center px-3 h-8 leading-tight text-gray-200 bg-gray-700 border border-gray-300 rounded-r-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-gray-700"
+                  }
+                >
+                  {index + 1} {/* Display the correct page number */}
+                </a>
+              ) : null}
+            </li>
+          ))}
+          <li>
+            <a
+              href="#"
+              className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-gray-700 ${
+                isLastPage ? "inactive-next" : "active-next"
+              }`}
+              onClick={() => handlePageChange(skip + limit)}
+            >
+              <span className="sr-only">Next</span>
+              <svg
+                className="w-2.5 h-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      ) : null}
+    </>
+  );
 };
 
 export default PaginationButton;

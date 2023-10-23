05239ed24436d5dbc6627d76e452ac1dfc28b12c
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-10-23 14:21:31 +0200

    bug fixeD

--- frontend/src/components/subComponents/PaginationButton.jsx
+++ frontend/src/components/subComponents/PaginationButton.jsx
@@ -46,11 +46,7 @@ const PaginationButton = () => {
           <a
             href="#"
             onClick={() => handlePageChange(index * limit)}
-            className={`${
-              index * limit === skip
-                ? "active-style flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-white"
-                : "inactive-style flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover-bg-gray-100 hover-text-gray-700 dark-bg-gray-800 dark-border-gray-700 dark-text-gray-400 dark-hover-bg-gray-700 dark-hover-text-white"
-            }`}
+            className={"bg-btton"}
           >
             {currentPage}
           </a>
